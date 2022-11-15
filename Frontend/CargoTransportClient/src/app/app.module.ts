7c386caafa043b93d60add9437d552ff46231774
vik37 <59177404+vik37@users.noreply.github.com>
2022-11-15 21:08:48 +0100

    Create default components and install bootstrap

--- Frontend/CargoTransportClient/src/app/app.module.ts
+++ Frontend/CargoTransportClient/src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { MainComponent } from './container/components/shared/main/main.component';
+import { HeaderComponent } from './container/components/shared/header/header.component';
+import { NavbarComponent } from './container/components/shared/navbar/navbar.component';
+import { FooterComponent } from './container/components/shared/footer/footer.component';
+import { HomeComponent } from './container/components/home/home.component';
+import { AboutComponent } from './container/components/about/about.component';
+import { ContactComponent } from './container/components/contact/contact.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainComponent,
+    HeaderComponent,
+    NavbarComponent,
+    FooterComponent,
+    HomeComponent,
+    AboutComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
